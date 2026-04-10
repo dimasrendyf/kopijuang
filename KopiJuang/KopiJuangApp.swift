793fb2df6242ae89939ae2b99609aad8f4d06bbd
dimasrendyf <dimasrendy55@gmail.com>
2026-04-10 08:53:23 +0700

    Initial Commit

---
+++ KopiJuang/KopiJuangApp.swift
@@ -0,0 +1,17 @@
+//
+//  KopiJuangApp.swift
+//  KopiJuang
+//
+//  Created by Dimas Rendy Febianto on 10/04/26.
+//
+
+import SwiftUI
+
+@main
+struct KopiJuangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
